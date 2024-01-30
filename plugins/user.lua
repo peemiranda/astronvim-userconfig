60f3a712faef4489b4a93d80aabe194ee46560bc
Caio Lesnock <caio.dev@outlook.com>
2024-01-30 13:45:48 -0300

    Add neoscroll

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,9 +8,18 @@ return {
     "nobbmaestro/nvim-andromeda",
     dependencies = { "tjdevries/colorbuddy.nvim", branch = "dev" }
   },
+
   {
     "styled-components/vim-styled-components",
     event = "User AstroFile"
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = "User AstroFile",
+    opts = {
+      mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+                '<C-y>', '<C-e>', 'zt', 'zz', 'zb'}
+    }
   },
 
   -- Disabled plugins
