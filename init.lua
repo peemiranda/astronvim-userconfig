038c08c9b2cd60248bc03fb47e2f4829b6a51512
Caio Lesnock <caio.dev@outlook.com>
2024-02-01 16:47:07 -0300

    Add php dap config

--- init.lua
+++ init.lua
@@ -83,6 +83,22 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+   local dap = require('dap')
+
+    dap.adapters.php = {
+      type = 'executable',
+      command = 'node',
+      args = { os.getenv('HOME') .. '/vscode-php-debug/out/phpDebug.js' }
+    }
+
+    dap.configurations.php = {
+      {
+        type = 'php',
+        request = 'launch',
+        name = 'Listen for Xdebug',
+        port = 9003
+      }
+    }
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
