e46d3415f671e529d07fc96d6f8d662c22f5c045
Caio Lesnock <caio.dev@outlook.com>
2024-02-25 14:54:43 -0300

    Remove highlights

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,30 +1,30 @@
 return { -- this table overrides highlights in all themes
   -- Transparency
-  Normal = { 
-    ctermbg = "none",
-  },
-  NonText = {
-    ctermbg = "none",
-  },
-  NeoTreeNormal = {
-    ctermbg = "none"
-  },
-  VertSplit = {
-    ctermbg = "none"
-  },
-  Title = {
-    ctermbg = "none"
-  },
-  NormalFloat = {
-    ctermbg = "none"
-  },
-  FloatBorder = {
-    ctermbg = "none"
-  },
-  PmenuSel = {
-    bg = "#ffff00"
-  },
-  FoldColumn = {
-    bg = "none"
-  }
+  -- Normal = { 
+  --   ctermbg = "none",
+  -- },
+  -- NonText = {
+  --   ctermbg = "none",
+  -- },
+  -- NeoTreeNormal = {
+  --   ctermbg = "none"
+  -- },
+  -- VertSplit = {
+  --   ctermbg = "none"
+  -- },
+  -- Title = {
+  --   ctermbg = "none"
+  -- },
+  -- NormalFloat = {
+  --   ctermbg = "none"
+  -- },
+  -- FloatBorder = {
+  --   ctermbg = "none"
+  -- },
+  -- PmenuSel = {
+  --   bg = "#ffff00"
+  -- },
+  -- FoldColumn = {
+  --   bg = "none"
+  -- }
 }
