b20c6c2f68e13742fdb483f4165909622bb587af
Caio Lesnock <caio.dev@outlook.com>
2024-02-26 08:32:45 -0300

    Add ensure installed treesitter langs

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,7 +5,13 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "http",
-        "json"
+        "json",
+        "javascript",
+        "typescript",
+        "css",
+        "scss",
+        "html",
+        "vue"
         -- "lua"
       })
     end,
