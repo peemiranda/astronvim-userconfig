73bab1dc17ec63e2facd44bb36ffcc29ea115825
Caio Lesnock <caio.dev@outlook.com>
2024-02-26 08:40:13 -0300

    Remove unnecessary themes

--- plugins/themes.lua
+++ plugins/themes.lua
@@ -1,22 +1,7 @@
 return {
   -- Themes
   {
-    "maxmx03/solarized.nvim",
-    opts = { transparent = true }
-  },
-  {
-    "nobbmaestro/nvim-andromeda",
-    dependencies = { "tjdevries/colorbuddy.nvim", branch = "dev" }
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
   },
-  {
-    "folke/tokyonight.nvim",
-    name = "tokyonight",
-    opts = {
-      style = "storm"
-    }
-  }
 }
