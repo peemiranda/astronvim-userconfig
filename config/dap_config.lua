379dbbebca7f702a531e66708a232a18c697337e
Caio Lesnock <caio@metadil.local@DSK-DESENV02.metadil.com.br>
2024-02-29 10:14:30 -0300

    Add php debug adapter

--- config/dap_config.lua
+++ config/dap_config.lua
@@ -3,14 +3,18 @@ local dap = require('dap')
 dap.adapters.php = {
   type = 'executable',
   command = 'node',
-  args = { os.getenv('HOME') .. '/vscode-php-debug/out/phpDebug.js' }
+  args = { os.getenv('HOME') .. '/.config/nvim/lua/user/adapters/vscode-php-debug/out/phpDebug.js' }
 }
 
+-- Do not forget to run 'npm install' and 'npm run build' inside the adapters/vscode-php-debug folder
 dap.configurations.php = {
   {
     type = 'php',
     request = 'launch',
     name = 'Listen for Xdebug',
-    port = 9003
+    port = 9003,
+    pathMappings = {
+        ['/var/www/html'] = "${workspaceFolder}"
+    },
   }
 }
