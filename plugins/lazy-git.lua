174f85f9d9a4a5acab2e5ef2f17162d1f92df92b
Pedro Henrique Miranda de Oliveira <pedro.henrique@metadil.com.br>
2024-04-02 14:50:12 -0300

    Adicionando lazy git e transparent

---
+++ plugins/lazy-git.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        requires = {
+            "nvim-lua/plenary.nvim",
+        },
+    }
+}
---
+++ plugins/transparent.lua
@@ -0,0 +1,22 @@
+return {
+    {
+      "xiyaowong/transparent.nvim",
+      lazy = false,
+      config = function()
+        require("transparent").clear_prefix("Neotree")
+        require("transparent").setup({
+            groups = { -- table: default groups
+              'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+              'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+              'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+              'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+              'EndOfBuffer',
+            },
+            extra_groups = {
+              "NeoTree"
+            }, 
+            exclude_groups = {},
+        })
+      end,
+    },
+}
